34688babb9a7aa3d77113666070f967902a699d7
dimasfhrz356 <dimasfahrezi356@gmail.com>
2025-05-08 00:01:48 +0700

    tambah tugas baru

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue';
+
+const listTugas = ref([]);
+const newTugas = ref('');
+
+const tambahTugas = () => {
+  if (newTugas.value.trim() !== '') {
+    listTugas.value.push(
+      {
+        id: Date.now(),
+        text: newTugas.value,
+        selesai: false,
+      }
+    )
+    newTugas.value = '';
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1>To Do List</h1>
+    <input type="text" v-model="newTugas" @keyup.enter="tambahTugas" placeholder="Tambah tugas">
+    <button @click="tambahTugas">Tambahkan</button>
+  </div>
+</template>
+
+<style scoped></style>
